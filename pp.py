427b3f36ece2a2e659edbb8e1c29269e03e75654
Newton Manyisa <manyisanewton26@gmail.com>
2025-03-25 14:22:04 +0300

    Python Operations with Flask Routing and Views

--- pp.py
+++ pp.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Python Operations with Flask Routing and Views</h1>"
-
+    return "<h1>Python Operations with Flask Routing and Views</h1>"    
 @app.route('/print/<string:param>')
 def print_string(param):
     print(param)  
-    return param  
-
+    return param
 @app.route('/count/<int:param>')
 def count(param):
     return "\n".join(str(i) for i in range(param)) + "\n"
@@ -33,6 +29,5 @@ def math(num1, operation, num2):
         return "Invalid operation"
     
     return str(result)
-
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True) 
